661f49b4407dd8eda403705a072a3d269b645d50
Jakub Janeczko <jjaneczk@gmail.com>
2016-06-08 22:02:09 +0200

    main.h: Disable LCD, enable DHCP, set RMII mode

--- inc/main.h
+++ inc/main.h
@@ -58,8 +58,8 @@
 /* Exported constants --------------------------------------------------------*/
 
 
-#define USE_LCD        /* enable LCD  */  
-//#define USE_DHCP       /* enable DHCP, if disabled static address is used */
+//#define USE_LCD        /* enable LCD  */  
+#define USE_DHCP       /* enable DHCP, if disabled static address is used */
 
 /* Uncomment SERIAL_DEBUG to enables retarget of printf to  serial port (COM1 on STM32 evalboard) 
    for debug purpose */   
@@ -92,34 +92,9 @@
 #define GW_ADDR2   0
 #define GW_ADDR3   1  
 
-/* MII and RMII mode selection, for STM324xG-EVAL Board(MB786) RevB ***********/
-//#define RMII_MODE  // User have to provide the 50 MHz clock by soldering a 50 MHz
-                     // oscillator (ref SM7745HEV-50.0M or equivalent) on the U3
-                     // footprint located under CN3 and also removing jumper on JP5. 
-                     // This oscillator is not provided with the board. 
-                     // For more details, please refer to STM3240G-EVAL evaluation
-                     // board User manual (UM1461).
+/* MII and RMII mode selection, for STM324xG-EVAL Board(MB786) RevB ***********/                                     
+#define RMII_MODE
 
-                                     
-#define MII_MODE
-
-/* Uncomment the define below to clock the PHY from external 25MHz crystal (only for MII mode) */
-#ifdef 	MII_MODE
- #define PHY_CLOCK_MCO
-#endif
-
-/* STM324xG-EVAL jumpers setting
-    +==========================================================================================+
-    +  Jumper |       MII mode configuration            |      RMII mode configuration         +
-    +==========================================================================================+
-    +  JP5    | 2-3 provide 25MHz clock by MCO(PA8)     |  Not fitted                          +
-    +         | 1-2 provide 25MHz clock by ext. Crystal |                                      +
-    + -----------------------------------------------------------------------------------------+
-    +  JP6    |          2-3                            |  1-2                                 +
-    + -----------------------------------------------------------------------------------------+
-    +  JP8    |          Open                           |  Close                               +
-    +==========================================================================================+
-  */
    
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */  
